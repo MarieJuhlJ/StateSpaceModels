8880b3a85fc8edcee3f2d13f9dddd81b0d8da0b2
MarieJuhlJ <121543742+MarieJuhlJ@users.noreply.github.com>
2025-03-03 10:33:33 +0100

    Intial code for model

--- src/ssm/model.py
+++ src/ssm/model.py
@@ -1,16 +1,40 @@
 from torch import nn
 import torch
+from ssm.hippo import make_DPLR_HiPPO
 
-class Model(nn.Module):
-    """Just a dummy model to show how to structure your code"""
-    def __init__(self):
-        super().__init__()
-        self.layer = nn.Linear(1, 1)
+class S4Layer(nn.Module):
+    def __init__(self, N, H, L):
+        super(S4Layer, self).__init__()
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        return self.layer(x)
+        self.N = N
+        self.H = H
+        self.L = L
 
-if __name__ == "__main__":
-    model = Model()
-    x = torch.rand(1)
-    print(f"Output shape of model: {model(x).shape}")
+        Lambda, P, B, _ = make_DPLR_HiPPO(N)
+        
+        self.P = nn.Parameter(self.P)
+        self.B = nn.Parameter(self.B)
+        self.Lambda = nn.Parameter(self.Lambda)
+
+        # Define C as a complex number with real and imaginary parts sampled from a normal distribution with mean 0 and std 0.5**0.5
+        self.C = nn.Parameter(torch.randn(N, 2) * 0.5**0.5)
+        self.C = self.C[:, 0] + 1j * self.C[:, 1]
+
+        self.step = 1e-3 # Should this be a learnable parameter?
+
+    def forward(self, x):
+        kernel_DPLR()
+
+
+
+
+
+
+
+class S4(nn.Module):
+    def __init__(self, input_dim, output_dim, H):
+        super(S4, self).__init__()
+
+        self.layers = nn.ModuleList([S4Layer(input_dim, output_dim) for _ in range(H)])
+
+
