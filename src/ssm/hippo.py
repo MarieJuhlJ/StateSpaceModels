92b3a8998ad07582bfe43ef7afb2c1bcc303ff0d
MarieJuhlJ <121543742+MarieJuhlJ@users.noreply.github.com>
2025-03-03 10:29:09 +0100

    Hippo using torch

--- src/ssm/hippo.py
+++ src/ssm/hippo.py
@@ -1,23 +1,21 @@
 """ Functions for creating hippo, NPLR hippo and DPLR hippo"""
 import numpy as np
 from scipy.linalg import eigh
-
+import torch
 
 def make_HiPPO(N):
-    P = np.sqrt(1 + 2 * np.arange(N))
-    A = P[:, np.newaxis] * P[np.newaxis, :]
-    A = np.tril(A) - np.diag(np.arange(N))
+    P = torch.sqrt(1 + 2 * torch.arange(N))
+    A = P[:, None] * P[None, :]
+    A = torch.tril(A) - torch.diag(torch.arange(N))
     return -A
 
 def make_NPLR_HiPPO(N):
-    # Make -HiPPO
+    # First make a HiPPO matrix
     nhippo = make_HiPPO(N)
-
-    # Add in a rank 1 term. Makes it Normal.
-    P = np.sqrt(np.arange(N) + 0.5)
-
-    # HiPPO also specifies the B matrix
-    B = np.sqrt(2 * np.arange(N) + 1.0)
+    
+    # Create P and B matrices
+    P = torch.sqrt(torch.arange(N) + 0.5)
+    B = torch.sqrt(2 * torch.arange(N) + 1.0)
     return nhippo, P, B
 
 def make_DPLR_HiPPO(N):
@@ -25,32 +23,34 @@ def make_DPLR_HiPPO(N):
     A, P, B = make_NPLR_HiPPO(N)
 
     # Construct the normal part of the normal + low rank matrix
-    S = A + P[:, np.newaxis] * P[np.newaxis, :]
+    S = A + P[:, None] * P[None, :]
 
     # Check skew symmetry
-    S_diag = np.diagonal(S)
-    Lambda_real = np.mean(S_diag) * np.ones_like(S_diag)
-    # assert np.allclose(Lambda_real, S_diag, atol=1e-3)
+    S_diag = S.diag()
+    Lambda_real = S_diag.mean() * torch.ones_like(S_diag)
 
     # Diagonalize S to V \Lambda V^*
     Lambda_imag, V = eigh(S * -1j)
+    V = torch.tensor(V, dtype=torch.cfloat)
 
-    P = V.conj().T @ P
-    B = V.conj().T @ B
+    P = V.conj().T @ (P+0j)
+    B = V.conj().T @ (B+0j)
     return Lambda_real + 1j * Lambda_imag, P, B, V
+
 
 def test_nplr(N=8):
     A2, P, B = make_NPLR_HiPPO(N)
     Lambda, Pc, Bc, V = make_DPLR_HiPPO(N)
     Vc = V.conj().T
-    P = P[:, np.newaxis]
-    Pc = Pc[:, np.newaxis]
-    Lambda = np.diag(Lambda)
+    P = P[:, None]
+    Pc = Pc[:, None]
+    Lambda = Lambda.diag()
 
     A3 = V @ Lambda @ Vc - (P @ P.T)  # Test NPLR
     A4 = V @ (Lambda - Pc @ Pc.conj().T) @ Vc  # Test DPLR
     assert np.allclose(A2, A3, atol=1e-4, rtol=1e-4)
     assert np.allclose(A2, A4, atol=1e-4, rtol=1e-4)
+    print("NPLR and DPLR tests passed")
 
 if __name__ == "__main__":
     test_nplr()
