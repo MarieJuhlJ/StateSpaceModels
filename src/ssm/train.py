d68c001d8cc1c6d60721d86c34fe5d4e2acd57b0
MarieJuhlJ <mariejuhljorgensen@gmail.com>
2025-03-10 13:45:09 +0100

    Tracking config with hydra

--- src/ssm/train.py
+++ src/ssm/train.py
@@ -1,16 +1,29 @@
 from ssm.model import S4Model
 from ssm.data import SMNIST
-import lightning
+import lightning as pl
 from torch.utils.data import DataLoader
+import hydra
+from omegaconf import DictConfig
 
-def train(N=64, H=128, L=784, num_blocks=4, class_out=10):
-    model = S4Model(N=N, H=H, L=L, num_blocks=num_blocks, cls_out=class_out)
+@hydra.main(config_name="config.yaml", config_path="../../configs")
+def train(cfg: DictConfig):
+    hp_config =cfg.experiment.hyperparameters
+    model = S4Model(N=hp_config.N, H=hp_config.H, L=hp_config.L, num_blocks=hp_config.num_blocks, cls_out=hp_config.class_out)
     
     dataset_train = SMNIST()
     dataset_val = SMNIST(train = False)
-    trainer = lightning.Trainer(accelerator="cpu", max_epochs=1)
-    train_dataloader = DataLoader(dataset_train, batch_size=32, shuffle=True, num_workers=4)
-    val_dataloader = DataLoader(dataset_val, batch_size=32, shuffle=False, num_workers=4)
+    train_dataloader = DataLoader(dataset_train, batch_size=hp_config.batch_size, shuffle=True, num_workers=4)
+    val_dataloader = DataLoader(dataset_val, batch_size=hp_config.batch_size, shuffle=False, num_workers=4)
+
+    checkpoint_callback = pl.pytorch.callbacks.ModelCheckpoint(dirpath="./models", monitor="val_loss", mode="min")
+
+    trainer = pl.Trainer(
+        accelerator="gpu", 
+        max_epochs=hp_config.max_epochs, 
+        logger=pl.pytorch.loggers.WandbLogger(project="ssm"), 
+        callbacks=[checkpoint_callback],
+        log_every_n_steps=hp_config.log_every_n_steps)
+
     trainer.fit(model, train_dataloader, val_dataloader)
 
 
