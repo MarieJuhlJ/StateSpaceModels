87ddaa68eb986360ab7ba309e605d1e0c0ed51e9
MarieJuhlJ <mariejuhljorgensen@gmail.com>
2025-05-13 10:49:38 +0200

    Training job for cross val

--- train.sh
+++ train.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
-#BSUB -J train[1-5]
-#BSUB -o logs/train_%J.out
-#BSUB -e logs/train_%J.err
+#BSUB -J trainConv512[1-5]
+#BSUB -o logs/trainConv512_%J.out
+#BSUB -e logs/trainConv512_%J.err
 #BSUB -q gpuv100
 #BSUB -n 4
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -R "rusage[mem=4G]"
 #BSUB -R "span[hosts=1]"
-#BSUB -W 120
+#BSUB -W 1080
 ####BSUB -u mariejuhljorgensen@gmail.com
 ####BSUB -B
 ####BSUB -N
@@ -25,4 +25,4 @@ module load cuda/11.6
 source /zhome/e3/b/155491/miniconda3/etc/profile.d/conda.sh
 conda activate ssm
 
-python src/ssm/train.py experiment=conv dataset=smnist k_folds=5 idx_fold=$LSB_JOBINDEX wandb=True
+python src/ssm/train.py experiment=conv experiment.hyperparameters.N=512 dataset=smnist k_folds=5 idx_fold=$LSB_JOBINDEX wandb=True experiment.hyperparameters.max_epochs=50
